f706f6e89ac709dedaf5595ac4f115801275e7b0
Michael Johnson <mj82@grnoc.iu.edu>
2015-08-26 18:32:51 +0000

    Add more detailed example config

--- config-management/puppet/perfsonar/manifests/site_rhel6.pp
+++ config-management/puppet/perfsonar/manifests/site_rhel6.pp
@@ -6,14 +6,77 @@ filebucket {
     path => "/var/lib/puppet/clientbucket/";
 }
 
+# The hostname of your syslog server
+$syslog_server = "syslog.test.com"
+
 node default {
-	include rhel6
+	class {'rhel6': 
+		web100 => true}
 	class {'perfsonar':
+		mesh => 'mesh1',
 		owamp => true,
-		bwctl => false
+		bwctl => false,
+		rsyslog_client => true,
+		rsyslog_server_host => $syslog_server,
+		rsyslog_server => false,
+		apache_enable => true,
+		#mesh_config_server => true,
+		# optional port definitions (if undefined, defaults are used)
+		#rsyslog_tcp_port => '600',
+		#http_port => 8000
 	}
-  	class {'rsyslog':
-		syslog_server => 'syslog.example.com' }
 }
 
+## host1
+node 'host1.test.com' {
+	include rhel6
+	class {'perfsonar':
+		mesh => 'mesh1',
+		owamp => true,
+		bwctl => true,
+		regular_testing => true,
+		ls_registration_daemon => true,
+		mesh_config_client => true,
+		mesh_config_server => false,
+		esmond => true,
+		esmond_pg_password => 'asdf',
+		rsyslog_server_host => $syslog_server,
+		#rsyslog_client => 'true',
+		#rsyslog_tcp_port => '600',
+		rsyslog_server => false,
+		rsyslog_udp => false,
+		#rsyslog_client => true,
+	}
+}
+
+# host2
+node 'host2.test.com' {
+	class {'rhel6': 
+		web100 => false}
+	class {'perfsonar':
+		mesh => 'testbed',
+		owamp => true,
+		bwctl => true,
+		rsyslog_client => true,
+		rsyslog_server_host => $syslog_server,
+		mesh_config_client => true,
+		rsyslog_server => true,
+		#rsyslog_tcp_port => '600',
+		#mesh_config_server => true,
+		#apache_enable => true,
+	}
+}
+
+# syslog server
+
+node 'syslog.test.com' {
+	class {'rhel6': 
+		web100 => false}
+	class {'perfsonar':
+		mesh => 'testbed',
+		owamp => true,
+		bwctl => true,
+		rsyslog_server => true,
+		#rsyslog_tcp_port => '600',
+	}
 }
